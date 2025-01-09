b37fe6c4dd50860b38985b77a011666f49088322
AlhaanAhmed-duet <Alhaanahmed123@gmail.com>
2025-01-09 08:21:35 +0500

    Removed godel encoding, making jsons for machine

--- src/config/PDA.tsx
+++ src/config/PDA.tsx
@@ -1,12 +1,12 @@
-import { ChangeEvent} from "react"
-
+import { useState } from "react"
 interface TransitionFunction {
     id: number,
     currState: string,
-    input: string | Array<string>,
+    onInputSymbol: string,
     tos: string,
-    operation: "push" | "pop" | "no push & pop",
-    nextState: string   
+    nextState: string,
+    operation: "push" | "pop" | "no action",
+    updatedTos: string
 }
 export class PushDownAutomata {
     private states: Array<string>
@@ -43,131 +43,34 @@ export class PushDownAutomata {
 
 }
 
-function readGodelFile(event: ChangeEvent<HTMLInputElement>): void {
-//  Import Files
-    let file = event.target.files?.[0]
-    const reader = new FileReader()
-    if (file) {
-        reader.onload = () => {
-            const content = reader.result as string
-            console.log(content)
-        }
-        reader.readAsText(file)
-    }
-}
-
-function processGodelData(): void {
-    const godelData: string = "101110111101101111"
-    let encodingRules = {
-        states: {
-            q0: "1",
-            q1: "11",
-            q2: "111",
-            q3: "1111"
-        },
-        inputSymbols: {
-            a: "1",
-            b: "11"
-        },
-        stackSymbols: {
-            epsilon: "111",
-            dollarSign: "1111"
-        }
-    }
-    const splitBySeperator = godelData.split("0")
-    const detectcurrentState = (): any => {
-        const statesEncoding = Object.values(encodingRules.states);
-        const checkStateEncoding = statesEncoding.find((str) => {
-            return str === splitBySeperator[0]
-        })
-        if (checkStateEncoding) {
-            const detectStatebyEncoding = Object.entries(encodingRules.states).find(([key, value]) => {
-                return value === checkStateEncoding
-            })
-            return detectStatebyEncoding?.[0]
-        }
-        else {
-            console.error("No state match is found, possibly wrong encoding!!")
-        }
-    }
-    const detectCurrentPrevInputSymbol = function (): any {
-        const detectCurrentInputSymbol: any = Object.entries(encodingRules.inputSymbols).find(([key,value]) => {
-            return value === splitBySeperator[1]
-        })
-        const detectCurrentStackSymbol: any = Object.entries(encodingRules.stackSymbols).find(([key , value]) => {
-            return value === splitBySeperator[1]
-        })
-        if (detectCurrentInputSymbol?.[0] == undefined && detectCurrentStackSymbol?.[0] != undefined) {
-            return detectCurrentStackSymbol?.[0]
-        }
-        else {
-            return detectCurrentInputSymbol?.[0]
-        }
-    }
-    const detectCurrentNextInputSymbol = function(): any {
-        const detectNextInputSymbol: any = Object.entries(encodingRules.inputSymbols).find(([key,value]) => {
-            return value === splitBySeperator[2]
-        })
-        const detectNextStackSymbol: any = Object.entries(encodingRules.stackSymbols).find(([key , value]) => {
-            return value === splitBySeperator[2]
-        })
-        if (detectNextInputSymbol?.[0] == undefined && detectNextStackSymbol?.[0] != undefined) {
-            return detectNextStackSymbol?.[0]
-        }
-        else {
-            return detectNextInputSymbol?.[0]
-        }
-    }
-    const detectNextState = function (): any {
-        const statesEncoding = Object.values(encodingRules.states);
-        const checkNextStateEncoding = statesEncoding.find((str) => {
-            return str === splitBySeperator[3]
-        })
-        if (checkNextStateEncoding) {
-            const detectStatebyEncoding = Object.entries(encodingRules.states).find(([key, value]) => {
-                return value === checkNextStateEncoding
-            })
-            return detectStatebyEncoding?.[0]
-        }
-        else {
-            console.error("No state match is found, possibly wrong encoding!!")
-        }
-    }
-    const detectNextInputSymbol = function (): any {
-        const detectNextInputIfInputSymbol = Object.entries(encodingRules.inputSymbols).find(([key, value]) => {
-            return value === splitBySeperator[4]
-        })
-        const detectNextInputIfStackSymbol = Object.entries(encodingRules.stackSymbols).find(([key, value]) => {
-            return value === splitBySeperator[4]
-        })
-        if (detectNextInputIfInputSymbol?.[0] === undefined && detectNextInputIfStackSymbol?.[0] !== undefined) {
-            return detectNextInputIfStackSymbol?.[0]
-        }
-        else {
-            return detectNextInputIfInputSymbol?.[0]
-        }
-    }
-    const currState = detectcurrentState()
-    const currSymbol = detectCurrentPrevInputSymbol()
-    const nextCurrSymbol = detectCurrentNextInputSymbol()
-    const nextState = detectNextState()
-    const nextInputSymbol = detectNextInputSymbol()
-    
-    console.log("Current State: ",currState)
-    console.log("Current Prev Input Symbol: ",currSymbol)
-    console.log("Current Next Input Symbol: ", nextCurrSymbol)
-    console.log("Next State: ",nextState)
-    console.log("Next Input Symbol: ", nextInputSymbol)
-}
-   
-
-
 export default function FileInputComponent() {
+    const emptyTransition: TransitionFunction = {
+        id: 0,
+        currState: "",
+        onInputSymbol: "",
+        tos: "",
+        nextState: "",
+        operation: "no action",
+        updatedTos: ""
+    }
+    const [newTransition, setNewTransition] = useState(emptyTransition)
+    
+    function createTransitions(): void {
+   
+    }
+    
     return (
         <>
-            <div>Upload a text file: </div>
-            <input type="file" onChange={readGodelFile} />
-            <button onClick={processGodelData}>Click Function</button>
+            <form>
+                <input type="text" placeholder="Enter current state: " />
+                <input type="text" placeholder="Enter input symbol: " />
+                <label>Enter operation on your input symbol: </label>
+                <input type="radio" name="operation"></input><label>Push</label>
+                <input type="radio" name="operation"></input><label>Pop</label>
+                <input type="radio" name="operation"></input><label>No action</label>
+                <input type="text" placeholder="Enter next state" />
+            </form>
+            <button>Generate PDA Machine</button>
         </>
     )
 }

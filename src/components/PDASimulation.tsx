46ae5db93d8f3624724587d8e66a50720bae522f
AlhaanAhmed-duet <Alhaanahmed123@gmail.com>
2025-01-12 14:48:14 +0500

    Remaining work will start from here

--- src/components/PDASimulation.tsx
+++ src/components/PDASimulation.tsx
@@ -1,5 +1,37 @@
 import { Link } from "react-router-dom"
+import { Transitions } from "../config/PDA"
 export default function PDASimulation() {
+    const transitions :Array<Transitions> = [{
+       currState: "q0", 
+       onInputSymbol: "a",
+       nextState: "q0",
+       operation: "push", 
+       updateTos: "a"
+    },
+    {
+        currState: "q0", 
+        onInputSymbol: "b",
+        nextState: "q1",
+        operation: "pop", 
+        updateTos: "epsilon"
+    },
+    {
+        currState: "q1", 
+        onInputSymbol: "b",
+        nextState: "q1",
+        operation: "pop", 
+        updateTos: "epsilon"
+    },
+    {
+        currState: "q1", 
+        onInputSymbol: "epsilon",
+        nextState: "q2",
+        operation: "pop", 
+        updateTos: "epsilon"
+    }]
+    let initialState = transitions[0].currState
+    let finalState = transitions[3].nextState
+// Remaining work will start from here
     return (
         <>
             
